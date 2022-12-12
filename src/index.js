441031fb6f726d7fdae117e8bdcd5c518b227ca4
DANNJUGUNA <ndan30071@gmail.com>
2022-12-12 11:53:30 +0300

    lab completed

--- src/index.js
+++ src/index.js
@@ -1,3 +1,23 @@
 document.addEventListener("DOMContentLoaded", () => {
-  // your code here
+  // your code here]
+  let form = document.querySelector("form")
+  form.addEventListener('submit', (e) => {
+      e.preventDefault()
+      handleTask(e.target.task.value)
+      form.reset()
+  })
 });
+function handleTask(task) {
+  let p = document.createElement('p');
+  let btn = document.createElement('button');
+  btn.addEventListener("click", handleDelete);
+  btn.textContent = "x"
+  p.textContent = `${task}`
+  p.appendChild(btn)
+  console.log(p);
+  document.querySelector("#list").appendChild(p);
+}
+
+function handleDelete(e) {
+  e.target.parentNode.remove()
+}
